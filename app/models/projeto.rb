a89ea53d6e6db2b6b1b86def376fc5b96c0715a1
Rodrigo Rosa <rodrigo@wonder.com.br>
2013-11-27 19:31:39 -0200

    Done

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -2,5 +2,6 @@ class Projeto < ActiveRecord::Base
 	belongs_to :user
 	has_many :comentarios
 
-	scope :most_recent, -> { order('created_at DESC') }
+	scope :mais_recentes, -> { order('created_at DESC') }
+  scope :mais_votados, -> {order()}
 end
